9b0ea112f17526292e29deaffa92f0a542c7c4d6
vicentcano <canorba@hotmail.es>
2023-06-12 02:40:43 +0200

    Added Top X Rocket Models

--- pages/3_Rocket_Models.py
+++ pages/3_Rocket_Models.py
@@ -26,31 +26,30 @@ def load_data():
     return launch_df
 
 
-def generate_pie(df, lower_bound, upper_bound):
-    grouped_un = df.groupby('Country').size().reset_index(name='counts')
-    grouped_un = grouped_un.sort_values(by='counts', ascending=False)
+def generate_bar(df, top):
+    country_counts = launch_df.groupby('Rocket').size().reset_index(name='count')
+    top_countries = country_counts.sort_values('count', ascending=False)['Rocket'].head(top)
+    df_top_countries = launch_df[launch_df['Rocket'].isin(top_countries)]
 
-    filtered_df = df[(df['Year'] >= lower_bound) & (df['Year'] <= upper_bound)]
-    grouped = filtered_df.groupby('Country').size().reset_index(name='counts')
-    grouped = grouped.sort_values(by='counts', ascending=False)
-    sns.set_style('darkgrid')
-    pallette_dict = {}
-    for country, color in zip(grouped_un['Country'].unique(), sns.color_palette("hls", 16).as_hex()):
-        pallette_dict[country] = color
-    fig, ax = plt.pie(grouped['counts'], colors=[pallette_dict.get(country, 'gray') for country in grouped['Country']])
-    plt.title('Launches by Country')
+    sns.set(style="darkgrid")
+    sns.set_palette("hls", 8)
+    ax = sns.barplot(x="Rocket", y="count", hue='Rocket', data=df_top_countries.groupby('Rocket').size().reset_index(name='count'), dodge=False, palette=sns.color_palette("hls", top))
 
-    plt.legend(labels=grouped['Country'], loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.set_title('Number of launches per rocket')
+    ax.set_xlabel("Rocket")
+    ax.set_xticklabels([])
+    ax.set_ylabel('Number of launches')
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
 
     st.pyplot(plt)
-    #plt.savefig("/home/vicent/miarfid/pid/graphs2/launches_by_country.svg", bbox_inches='tight')
     plt.clf()
 
-st.set_page_config(page_title="Space Race Pie Chart", page_icon="🚀")
-st.markdown("# Space Race visualized in a Pie Chart")
-
-values = st.slider('Select a range of values', 1957, 2022, (1957, 2022))
+st.set_page_config(page_title="Top X rocket models", page_icon="🚀")
+st.markdown("# Top X rocket models")
+st.sidebar.header("Top X rocket models")
 
 launch_df = load_data()
-generate_pie(launch_df, values[0], values[1])
 
+value = st.slider('Select an X to create a Top X', 3, 25, 10)
+
+generate_bar(launch_df, value)
